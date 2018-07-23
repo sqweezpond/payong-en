720d1a0c09d4e2a87d57cae259b48d7eccc179bd
Parinya Yodming <sqweezpond>
2018-07-23 17:06:53 +0700

    update
    update (pond)

--- inc/header.php
+++ inc/header.php
@@ -1,4 +1,4 @@
-    <title>Payong</title>
+    <title>Payong EN</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
--- index.php
+++ index.php
@@ -15,9 +15,32 @@
        <!-- CONTENT -->
 
         <div class="container pb-5">
-            <div class="row">
-              <div class="col-12">
-
+            <div class="row justify-content-center home-p-t">
+              <div class="col-xl-5">
+                <div class="card b-n">
+                    <div class="card-top home-card">
+                        <div class="row">
+                            <div class="col-xl-8">
+                                <div class="home-font">Private Horoscope</div>
+                            </div>
+                            <div class="col-xl-4">
+                                <div class="home-overlay-image">
+                                   <img src="assets/images/pay/pp.jpg" class="home-image-full">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body home-padding">
+                            <div class="home-overlay-image2">
+                                 <img src="assets/images/logo/write.jpg" class="home-image-full2">
+                            </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div class="row mt-5">
+              <div class="col-12 text-center">
+                <button type="button" class="btn home-btn">About Payong</button>
               </div>
             </div>
 
