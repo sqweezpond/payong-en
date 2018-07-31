524a532d5da077188e9a2d2ebe136f494856b4f3
Parinya Yodming <sqweezpond>
2018-07-31 08:55:28 +0700

    update
    update (pond)

--- pay-confirm.php
+++ pay-confirm.php
@@ -15,7 +15,7 @@
        <!-- CONTENT -->
 
        <div class="container">
-<<<<<<< HEAD
+
              <div class="row">
                  <div class="col-lg-12">
                      <div class="row">
@@ -70,82 +70,6 @@
                     </div>
                  </div>
              </div>
-=======
-         <div class="row">
-           <div class="col-lg-12">
-             <div class="my-4 text-center">
-                <h4 class="pay-con-font-top ">Please review and comfirm information correctly,then click 'Continue to Paypal'</h4>
-             </div>
-             <div class="row pay-con-fix-lift-1024">
-                 <div class="col-lg-6 col-6">
-                    <p class="text-right pay-con-font-top-1">E-mail to receive the document <span class="ml-4">:</span></p>
-                 </div>
-                 <div class="col-lg-6 col-6">
-                    <p class="text-left pay-con-font-top-1"> Userprivate@gmail.com</p>
-                 </div>
-                 <div class="col-lg-6 col-6">
-                    <p class="text-right pay-con-font-top-1">Date of Birth <span class="ml-4">:</span> </p>
-                 </div>
-                 <div class="col-lg-6 col-3">
-                    <p class="text-left pay-con-font-top-1"> 1/12/2018</p>
-                 </div>
-                 <div class="col-lg-6 col-6">
-                    <p class="text-right pay-con-font-top-1">Time of Birth <span class="ml-4">:</span></p>
-                 </div>
-                 <div class="col-lg-6 col-6">
-                    <p class="text-left pay-con-font-top-1"> 23.00</p>
-                 </div>
-                 <div class="col-lg-6 col-6">
-                    <p class="text-right pay-con-font-top-1">Country of Birth <span class="ml-4">:</span></p>
-                 </div>
-                 <div class="col-lg-6 col-6">
-                    <p class="text-left pay-con-font-top-1"> Afghanistan</p>
-                 </div>
-                 <div class="col-lg-6 col-6">
-                    <p class="text-right pay-con-font-top-1">State of Birth <span class="ml-4">:</span></p>
-                 </div>
-                 <div class="col-lg-6 col-6">
-                    <p class="text-left pay-con-font-top-1">Kebul</p>
-                 </div>
-                 <div class="col-lg-6 col-6">
-                    <p class="text-right pay-con-font-top-1">Gender <span class="ml-4">:</span></p>
-                 </div>
-                 <div class="col-lg-6 col-6">
-                     <p class="text-left pay-con-font-top-1">Men</p>
-                 </div>
-                 <div class="col-lg-6 col-6">
-                     <p class="text-right pay-con-font-top-1">Fullname or nickname <span class="ml-4">:</span></p>
-                 </div>
-                 <div class="col-lg-6 col-6">
-                     <p class="text-left pay-con-font-top-1">User</p>
-                 </div>
-              </div>
-              <div class="col-xl-12  text-lg-right text-center col-12 my-5">
-                <button type="submit" class="btn paycon-btn">Contiule to Paypal</button>
-              </div>
-           </div>
-         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-       </div>
->>>>>>> 961ec41733155601859f97db35c54ebc9f4f5fe3
 
            <!-- END CONTANER ------>
        </div>
